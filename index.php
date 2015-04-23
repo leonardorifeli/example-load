fd8356c965ed98f16ecf465fb012cd07fa78e0cc
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-04-23 00:55:56 -0300

    Started

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Example to load select change</title>
+</head>
+<body>
+
+</body>
+<script src="js/jquery-1.11.2.js"></script>
+<script src="js/system.js"></script>
+</html>
